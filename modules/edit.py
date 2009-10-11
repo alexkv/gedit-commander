9cb35a438a470e26cabc30211ca8f0ccc177e347
Jesse van den Kieboom <jesse@icecrew.nl>
2009-10-11 02:29:36 +0200

    Added file renaming

--- modules/edit.py
+++ modules/edit.py
@@ -45,7 +45,65 @@ def __default__(filename, view):
 		
 	return commander.commands.result.HIDE
 
-locals()['file'] = __default__
+def _dummy_cb(num, total):
+	pass
+
+def rename(view, newfile):
+	"""Rename current file: edit.rename &lt;newname&gt;"""
+	
+	doc = view.get_buffer()
+	
+	if not hasattr(doc, 'set_uri'):
+		raise commander.commands.exceptions.Execute('Your version of gedit does not support this action')
+	
+	if doc.is_untitled():
+		raise commander.commands.exceptions.Execute('Document is unsaved and thus cannot be renamed')
+	
+	if doc.get_modified():
+		raise commander.commands.exceptions.Execute('You have unsaved changes in your document')
+	
+	if not doc.is_local():
+		raise commander.commands.exceptions.Execute('You can only rename local files')
+	
+	f = gio.File(doc.get_uri())
+	
+	if not f.query_exists():
+		raise commander.commands.exceptions.Execute('Current document file does not exist')
+	
+	if os.path.isabs(newfile):
+		dest = gio.File(newfile)
+	else:
+		dest = f.get_parent().resolve_relative_path(newfile)
+	
+	if f.equal(dest):
+		yield commander.commands.result.HIDE
+	
+	if not dest.get_parent().query_exists():
+		# Check to create parent directory
+		fstr, words, modifierret = (yield commands.result.Prompt('Directory does not exist, create? [Y/n] '))
+		
+		if fstr.strip().lower() in ['y', 'ye', 'yes', '']:
+			# Create parent directories
+			try:
+				os.makedirs(dest.get_parent().get_path())
+			except OSError, e:
+				raise commander.commands.exceptions.Execute('Could not create directory')
+		else:
+			yield commander.commands.result.HIDE
+	
+	if dest.query_exists():
+		fstr, words, modifierret = (yield commands.result.Prompt('Destination already exists, overwrite? [Y/n]'))
+		
+		if not fstr.strip().lower() in ['y', 'ye', 'yes', '']:
+			yield commander.commands.result.HIDE
+	
+	try:
+		f.move(dest, _dummy_cb, flags=gio.FILE_COPY_OVERWRITE)
+		
+		doc.set_uri(dest.get_uri())
+		yield commander.commands.result.HIDE
+	except Exception, e:
+		raise commander.commands.exceptions.Execute('Could not move file: %s' % (e,))
 
 def _mod_has_func(mod, func):
 	return func in mod.__dict__ and type(mod.__dict__[func]) == types.FunctionType
@@ -110,17 +168,19 @@ def command(view, name):
 	
 	raise commander.commands.exceptions.Execute('Could not find command: ' + name)
 
-def new_command(view, name, **kwargs):
+def new_command(view, entry, name):
 	"""Create a new commander command module: edit.new-command &lt;command&gt;"""
 	
 	filename = os.path.expanduser('~/.gnome2/gedit/commander/modules/' + name + '.py')
 	
 	if os.path.isfile(filename):
-		raise commander.commands.exceptions.Execute('Commander module ' + name + ' already exists')
+		raise commander.commands.exceptions.Execute('Commander module `' + name + '\' already exists')
 	
 	f = open(filename, 'w')
-	f.write("import commands\n\n__commander_module__ = True\n\ndef __default__(view, *args, **kwargs):\n\t\"\"\"Some kind of cool new feature: cool &lt;something&gt;\n\nUse this to apply the cool new feature\"\"\"\n\tpass\n")
+	f.write("import commander.commands\n\n__commander_module__ = True\n\ndef __default__(view, entry):\n\t\"\"\"Some kind of cool new feature: cool &lt;something&gt;\n\nUse this to apply the cool new feature\"\"\"\n\tpass\n")
 	f.close()
 	
-	kwargs['_cmd'] = filename
-	return __default__(view, filename, **kwargs)
+	return __default__(filename, view)
+
+locals()['file'] = __default__
+move = rename
